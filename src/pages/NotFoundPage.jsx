4635c3b7a55a1d35a09265b85cce4f158b78ff4e
Andrey Babichev <klopipa@gmail.com>
2023-07-04 11:14:49 +0300

    fix not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,11 @@
-import Login from "../components/Login";
+import { Link } from "react-router-dom";
 
 export default function NotFoundPage() {
   return (
     <div className="not_found_page">
       <h1>404</h1>
       <h3>Not found page</h3>
+      <Link to="/">Go Home</Link>
     </div>
   );
 };
